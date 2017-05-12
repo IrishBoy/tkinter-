761277ff7ca6ef8b1a579ec6dc90ada7a900aff4
IrishBoy <mamilyaev.m.s@gmail.com>
2017-05-12 17:22:57 +0400

    Update game.py

--- game.py
+++ game.py
@@ -1,9 +1,13 @@
 from tkinter import *
 import random 
+import time
 
 tk = Tk()
+win_width = 360
+win_height = 380
+config_string = "{0}x{1}".format(win_width, win_height + 32)
 tk.title("Just a game 4 fun")
-canvas = Canvas(tk, width=550, height=400, bd=0, highlightthickness=0)
+canvas = Canvas(tk, width=550, height=400, bd=5, highlightthickness=0)
 canvas.pack()
 tk.update()
 
