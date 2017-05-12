a32d67512f87ecfd13b3ea13d8a2d9785dba27e2
IrishBoy <mamilyaev.m.s@gmail.com>
2017-05-12 17:16:04 +0400

    Add files via upload

---
+++ game.py
@@ -0,0 +1,9 @@
+from tkinter import *
+import random 
+
+tk = Tk()
+tk.title("Just a game 4 fun")
+canvas = Canvas(tk, width=550, height=400, bd=0, highlightthickness=0)
+canvas.pack()
+tk.update()
+
